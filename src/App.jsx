83d5598a7e3dc0a3ec9e3c7ea65f3691c0691e73
Park Sung Hwa <sihus@naver.com>
2021-04-14 11:09:46 +0900

    up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            <h1>React on webpack</h1>
+            <p>Non Create React App Build</p>
+            <p>Non Create React App Build</p>
+            <p>Non Create React App Build</p>
+        </div>
+    )
+}
+
+export default App
